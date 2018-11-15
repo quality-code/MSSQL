1420cbd0772a8535603d30f165827fc07fba34ba
Quality Code S.A.S <44443286+quality-code@users.noreply.github.com>
2018-11-14 20:28:40 -0500

    From URL using Storage Account Access Key

--- Utilities/Backup with Storage Account Identity and Access Key.sql
+++ Utilities/Backup with Storage Account Identity and Access Key.sql
@@ -9,9 +9,19 @@ WITH CREDENTIAL = 'backupSql',
 	MEDIANAME = 'MigrationBackups',
 	NAME = 'Full Backup of ExampleDB'
 
+--From URL using Storage Account Access Key
 RESTORE DATABASE AndresT 
 FROM URL = 'https://bk20765.blob.core.windows.net/backups/data2.bak'
-WITH  CREDENTIAL = 'backupSql',
-	REPLACE,
-	MOVE 'TSQL' TO 'T:\ExampleDB2.mdf', 
-	MOVE 'TSQL_log' TO 'T:\ExampleDB_log2.ldf';
+WITHÂ  CREDENTIAL = 'backupSql'
+	,REPLACE
+	,MOVE 'TSQL' TO 'T:\ExampleDB2.mdf'
+	,MOVE 'TSQL_log' TO 'T:\ExampleDB_log2.ldf'
+	,NORECOVERY  
+	,STATS = 5;
+	
+RESTORE LOG AdventureWorks2016 
+FROM URL = 'https://bk20765.blob.core.windows.net/backups/data.trn'   
+WITH CREDENTIAL = 'backupSql'
+	,RECOVERY  
+	,STOPAT = 'May 18, 2015 5:35 PM'   
+GO  
