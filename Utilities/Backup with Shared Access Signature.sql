c6a11b87e5c66b56136d4690f044f615c17f3d38
Quality Code S.A.S <44443286+quality-code@users.noreply.github.com>
2018-11-14 20:26:01 -0500

    Restoring to a point-in-time using STOPAT

--- Utilities/Backup with Shared Access Signature.sql
+++ Utilities/Backup with Shared Access Signature.sql
@@ -9,8 +9,19 @@ WITH COMPRESSION, FORMAT,
 	MEDIANAME = 'MigrationBackups',
 	NAME = 'Full Backup of ExampleDB'
 
+--Restoring to a point-in-time using STOPAT
 RESTORE DATABASE Andres 
 FROM URL = 'https://bk20765.blob.core.windows.net/backups/data.bak'
-WITH  REPLACE,
-	MOVE 'TSQL' TO 'T:\ExampleDB.mdf', 
-	MOVE 'TSQL_log' TO 'T:\ExampleDB_log.ldf';
+WITHÂ  REPLACE
+	,NORECOVERY  
+	,REPLACE  
+	,STATS = 5
+	,MOVE 'TSQL' TO 'T:\ExampleDB.mdf'
+	,MOVE 'TSQL_log' TO 'T:\ExampleDB_log.ldf';
+	
+RESTORE LOG AdventureWorks2016 
+FROM URL = 'https://bk20765.blob.core.windows.net/backups/data.trn'   
+WITH   
+RECOVERY   
+,STOPAT = 'May 18, 2015 5:35 PM'   
+GO  
